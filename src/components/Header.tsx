c4ef9e6d9646ca50913d58bd695232b7baf71431
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-03-11 14:01:45 +0300

    Styling the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container } from '../styles/Container';
 import { HeaderStyle } from '../styles/Header';
 
 function Header() {
     return (
-      <Container>
-        <HeaderStyle>
-          <h1 data-testid="header-title">Where in the world?</h1>
+      <HeaderStyle>
+        <Container>
+          <Link to="/">
+            <h1 data-testid="header-title">Where in the world?</h1>
+          </Link>
           <p data-testid="header-mode">Dark Mode</p>
-        </HeaderStyle>
-      </Container>
+        </Container>
+      </HeaderStyle>
     )
 }
 
--- src/styles/Header.tsx
+++ src/styles/Header.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import styled from 'styled-components';
 
 export const HeaderStyle = styled.header`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+    background-color: white;
+
+    a {
+        color: currentColor;
+        text-decoration: none;
+    }
+
+    > * {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
 `;
