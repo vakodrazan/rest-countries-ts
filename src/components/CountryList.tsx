91bdea49b809d7d95e2ca0dde7460cf26c8d11ea
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-03-11 15:25:59 +0300

    Styling the country list

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { Container } from '../styles/Container';
+import { CountryListStyle } from '../styles/CountryList';
 import FilterCountries from './FilterCountries';
 import { GlobalContext } from './GlobalContext'
 
@@ -10,21 +11,30 @@ function CountryList() {
     return (
         <Container data-testid="wrapper">
             <FilterCountries />
-            <ul data-testid="country-list">
+            <CountryListStyle data-testid="country-list">
                 {countries.map((country) => (
-                    <li data-testid="country-item" key={country.numericCode}>
+                    <li className="country-list" data-testid="country-item" key={country.numericCode}>
                         <Link to={`/country/${country.alpha3Code}`}>
                             <img src={country.flag} alt={`${country.name} flag`} />
-                            <h2 data-testid="country-title">{country.name}</h2>
-                            <div data-testid="country-content">
-                                <p data-testid="country-population">{country.population}</p>
-                                <p data-testid="country-region">{country.region}</p>
-                                <p data-testid="country-capital">{country.capital}</p>
+                            <h2 className="country-list-heading" data-testid="country-title">{country.name}</h2>
+                            <div className="country-list-content" data-testid="country-content">
+                                <p className="country-list-content-detail" data-testid="country-population">
+                                    Population: 
+                                    <span className="country-list-content-detail-item"> {country.population}</span>
+                                </p>
+                                <p className="country-list-content-detail" data-testid="country-region">
+                                    Region: 
+                                    <span className="country-list-content-detail-item"> {country.region}</span>
+                                </p>
+                                <p className="country-list-content-detail" data-testid="country-capital">
+                                    Capital:
+                                    <span className="country-list-content-detail-item"> {country.capital}</span>
+                                </p>
                             </div>
                         </Link>
                     </li>
                 ))}
-            </ul>
+            </CountryListStyle>
         </Container>
     )
 }
---
+++ src/styles/CountryList.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const CountryListStyle = styled.ul`
+    display: grid;
+    row-gap: 44px;
+    column-gap: 50px;
+    justify-content: center;
+    grid-template-columns: repeat(auto-fit, 320px);
+    text-align: left;
+
+    a {
+        color: currentColor;
+        text-decoration: none;
+    }
+
+    .country-list {
+        border-radius: 8px;
+        box-shadow: 0 0 0.7rem 0.2rem rgb(0 0 0 / 3%);
+        transition: var(--transition) transform, var(--transition) box-shadow;
+
+        &-heading {
+            padding-left: 32px;
+            padding-right: 32px;
+        }
+        &-content {
+            padding-top: 32px;
+            padding-left: 32px;
+            padding-right: 32px;
+            padding-bottom: 44px;
+            transition: var(--transition) background-color;
+
+            &-detail {
+                font-weight: var(--weight-semi-bold);
+
+                &-item {
+                    font-weight: var(--weight-light);
+                }
+            }
+        }
+
+        img {
+            width: 100%;
+            height: 214px;
+            object-fit: cover;
+        }
+    }
+
+`;
