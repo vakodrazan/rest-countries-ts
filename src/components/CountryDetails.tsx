45ab3b6b5053deaa797fa67c1ece7ed281f2bc47
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-04-01 09:50:32 +0300

    Carry working on the application

--- src/components/CountryDetails.tsx
+++ src/components/CountryDetails.tsx
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom';
 import { Container } from '../styles/Container';
+import { CountryDetailListStyle, CountryDetailsStyle } from '../styles/CountryDetails';
 import { GlobalContext } from './GlobalContext';
 
 function CountryDetails() {
@@ -17,47 +18,45 @@ function CountryDetails() {
 
     return (
         <Container>
-            <Link to="/">
-                <button>Back</button>
-            </Link>
-            <section>
-                <img src={findCountry?.flag} alt={`${findCountry?.name} flag`} />
-                <div>
+            <CountryDetailListStyle>
+                <Link to="/" className="call-to-action">
+                    <span>Back</span>
+                </Link>
+                <CountryDetailsStyle>
+                    <img src={findCountry?.flag} alt={`${findCountry?.name} flag`} />
                     <div>
                         <div>
-                            <p>Native Name: {findCountry?.nativeName}</p>
-                            <p>Population: {findCountry?.population}</p>
-                            <p>Region: {findCountry?.region}</p>
-                            <p>Sub Region: {findCountry?.subregion}</p>
-                            <p>Capital: {findCountry?.capital}</p>
+                            <div>
+                                <p>Native Name: {findCountry?.nativeName}</p>
+                                <p>Population: {findCountry?.population?.toLocaleString("en-US")}</p>
+                                <p>Region: {findCountry?.region}</p>
+                                <p>Sub Region: {findCountry?.subregion}</p>
+                                <p>Capital: {findCountry?.capital}</p>
+                            </div>
+                            <div>
+                                <p>Top Level Domain: {findCountry?.topLevelDomain?.map(topLevel => (topLevel)).join(', ')}</p>
+                                <p>Currencies: {findCountry?.currencies?.map(currencie => (
+                                    <span key={currencie.name}>{currencie.name}</span>
+                                ))}</p>
+                                <p>Languages: {findCountry?.languages?.map((language) => (language?.name)).join(', ')}</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>Top Level Domain: {findCountry?.topLevelDomain?.map(topLevel => (
-                                <span key={topLevel}>{topLevel}</span>
-                            ))}</p>
-                            <p>Currencies: {findCountry?.currencies?.map(currencie => (
-                                <span key={currencie.name}>{currencie.name}</span>
-                            ))}</p>
-                            <p>Languages: {findCountry?.languages?.map((language, index) => (
-                                <span key={language.iso639_1}>{(index ? ', ' : '') + language?.name}</span>
-                            ))}</p>
-                        </div>
-                    </div>
-                    <p>Border Countries: 
-                        {findCountry?.borders?.length ? findCountry?.borders?.map(border => {
-                        const findBorderCountry = countries.find(country => country.alpha3Code === border);
+                        <p>Border Countries: 
+                            {findCountry?.borders?.length ? findCountry?.borders?.map(border => {
+                            const findBorderCountry = countries.find(country => country.alpha3Code === border);
 
-                            return (
-                                <Link to={`/country/${border}`} key={border}>
-                                    <span> {findBorderCountry?.name} </span>
-                                </Link> 
-                            )
-                        })
-                        : <span>No border countries for this country.</span>
-                    }
-                    </p>
-                </div>
-            </section>
+                                return (
+                                    <Link to={`/country/${border}`} key={border}>
+                                        <span> {findBorderCountry?.name} </span>
+                                    </Link> 
+                                )
+                            })
+                            : <span>No border countries for this country.</span>
+                        }
+                        </p>
+                    </div>
+                </CountryDetailsStyle>
+            </CountryDetailListStyle>
         </Container>
     )
 }
---
+++ src/styles/CountryDetails.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const CountryDetailListStyle = styled.article`
+    padding-top: 54px;
+    .call-to-action {
+        box-shadow: 0 0 0.7rem 0 rgb(0 0 0 / 29%);
+        border-radius: .2rem;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        text-align: left;
+        align-items: center;
+        display: inline-flex;
+        justify-content: center;
+        text-decoration: none;
+        color: currentColor;
+    }
+`;
+export const CountryDetailsStyle = styled.section`
+    padding-top: 54px;
+`;
