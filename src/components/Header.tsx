a3db8c095a680fa8ac6d3ce290ed85053ae35b97
Noeline <noeline.mar@onja.org>
2021-07-24 15:04:26 +0300

    Styling the header page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { DarkModeIcon, LightModeIcon } from '../icons/Icons'
 import { Container } from '../styles/Container'
-import { ButtonMode, HeaderStyle } from '../styles/Header'
+import { ButtonMode, HeaderStyle, Heading } from '../styles/Header'
 import { GlobalContext } from './GlobalContext'
 
 function Header() {
@@ -12,7 +12,7 @@ function Header() {
     <HeaderStyle>
       <Container>
         <Link to='/'>
-          <h1 className='heading'>Where in the world?</h1>
+          <Heading className='heading'>Where in the world?</Heading>
         </Link>
         <ButtonMode onClick={toggleTheme}>
           {themeMode ? LightModeIcon : DarkModeIcon}{' '}
--- src/styles/Header.js
+++ src/styles/Header.js
@@ -1,29 +1,35 @@
 import styled from 'styled-components'
+import { Container } from './Container'
 
 export const HeaderStyle = styled.header`
   background-color: ${({ theme }) => theme.background};
   box-shadow: var(--color-blurry) 0 0 0.7rem 0.2rem;
-  padding-top: 20px;
-  padding-bottom: 7px;
+  padding-top: 30px;
+  padding-bottom: 15px;
 
   a {
     color: currentColor;
     text-decoration: none;
   }
 
-  .heading {
-    font-size: 18px;
-
-    @media (min-width: 650px) {
-      font-size: 24px;
-    }
-  }
-
-  > * {
+  > ${Container} {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+  }
+`
+
+export const Heading = styled.h1`
+  font-size: 19px;
+  font-weight: 800;
+
+  @media (min-width: 400px) {
+    font-size: 24px;
+  }
+
+  @media (min-width: 750px) {
+    font-size: 28px;
   }
 `
 
